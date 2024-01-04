5ed047803eb5c09f243ab1cc90d345dffe45786e
RichardKim <hiddeneventor@gmail.com>
2024-01-04 22:50:57 +0900

    initial book-server

---
+++ src/test/java/me/ricky/boardserver/BoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ricky.boardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
